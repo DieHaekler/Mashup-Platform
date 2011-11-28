4509f5e0332c15ddc1265669c90b345da7117d9b
MalteW <smaraph@gmail.com>
2011-11-28 20:13:18 +0100

    initial

---
+++ MP_final/src/ch/ffhs/inf09/pa/mashup_platform/web/model/ModelMashups.java
@@ -0,0 +1,32 @@
+package ch.ffhs.inf09.pa.mashup_platform.web.model;
+
+import ch.ffhs.inf09.pa.mashup_platform.common.db.*;
+
+public class ModelMashups extends ModelApplication
+{	
+	public ModelMashups(DBLocal db)
+	{
+		super(db);
+	}
+	
+	public Mashups get(int start, int number, int sortedBy, int status,
+			String username)
+	{
+		return db.getMashups(start, number, sortedBy, status, username);
+	}
+	
+	public Mashups get(int start, int number, int sortedBy, int status)
+	{
+		return db.getMashups(start, number, sortedBy, status);
+	}
+	
+	public Mashups get(int start, int number, int sortedBy)
+	{
+		return db.getMashups(start, number, sortedBy);
+	}
+	
+	public Mashups get(int start, int number)
+	{
+		return db.getMashups(start, number);
+	}
+}
---
+++ MP_final/src/ch/ffhs/inf09/pa/mashup_platform/web/model/ModelUser.java
@@ -0,0 +1,21 @@
+package ch.ffhs.inf09.pa.mashup_platform.web.model;
+
+import ch.ffhs.inf09.pa.mashup_platform.common.db.*;
+
+public class ModelUser extends ModelApplication
+{
+	private User user;
+	
+	public ModelUser(DBLocal db, String username, String password)
+	{
+		super(db);
+		user = db.getUser(username, password);
+	}
+	
+	public boolean doesExist()
+	{
+		return user != null;
+	}
+	
+	public User getUser() { return user; }
+}
