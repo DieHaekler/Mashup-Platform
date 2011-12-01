110a612ff87f16e69a1824fe9d1c7f6df35746e4
Joël Poyet <joel@poyet.ch>
2011-12-01 14:22:36 +0100

    untrack config.java

--- MP_final/src/ch/ffhs/inf09/pa/mashup_platform/config/Config.java
+++ MP_final/src/ch/ffhs/inf09/pa/mashup_platform/config/Config.java
@@ -2,7 +2,7 @@ package ch.ffhs.inf09.pa.mashup_platform.config;
 
 public class Config
 {
-	public static final String FILE_PATH_SYSTEM = "C:/Users/jwild/git/Mashup-Platform/MP_final/";
+	public static final String FILE_PATH_SYSTEM = "D:/Eigene Dateien/Schule/FFHS/PA/GitHub_Mashup-Platform/Mashup-Platform/MP_final/";
 	public static final String WEB_PATH_ROOT = "/MP_final/";
 	public static final String FILE_PATH_OUTPUT = FILE_PATH_SYSTEM + "output/";
 	public static final String FILE_PATH_CACHE = FILE_PATH_SYSTEM + "cache/";
